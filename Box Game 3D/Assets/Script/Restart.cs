e37187c5be1f50ee113414f40bf47bf5403c396b
Sameer Khowaja <sameerkhowaja12@gmail.com>
2019-12-31 19:29:44 +0500

    Add Completed FIle

---
+++ Box Game 3D/Assets/Script/Restart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public Text TScore;
+    private string st;
+
+    private void Start()
+    {
+       st = TScore.text;
+        TScore.text = st + "\nPress Spacebar to Restart";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+            Invoke("Restarter", 0.5f);
+    }
+
+    void Restarter()
+    {
+        SceneManager.LoadScene("lvl01");
+    }
+    
+}
---
+++ Box Game 3D/Assets/Script/Scores.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scores : MonoBehaviour
+{
+    public float timer;
+    public float score;
+    public Text TScore;
+    public Scores sc;
+    public Restart rst;
+    public enemyMovement em;
+    private string Fscore;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        timer += Time.deltaTime;
+
+        if (timer > 1f)
+        {
+            Fscore = TScore.text;
+            score += 1;
+            //We only need to update the text if the score changed.
+            TScore.text = "SCORES : " + score.ToString();
+            //Reset the timer to 0.
+            timer = 0;
+
+            if (em.force == 0f)
+            {
+                TScore.text = Fscore;
+                rst.enabled = true;
+                sc.enabled = false;
+            }
+        }
+        
+    }
+
+}
